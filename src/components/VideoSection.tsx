a7b9d4de69b69718da455a2e6ecc0f6317ba32d7
cnguyen111 <cnguyen1@wpi.edu>
2025-03-06 11:47:33 -0500

    Vlog structure

--- src/components/VideoSection.tsx
+++ src/components/VideoSection.tsx
@@ -1,112 +1,113 @@
-import {
-    Timeline, TimelineBody,
-    TimelineConnector,
-    TimelineHeader,
-    TimelineIcon,
-    TimelineItem,
-    Typography
-} from "@material-tailwind/react";
-import { ClockIcon } from "@heroicons/react/24/outline";
-
-
 export default function VideoSection() {
     return (
 
-        <div className="h-full w-full overflow-y-auto py-20">
-            <Timeline className="flex flex-col justify-center items-center">
-                <TimelineItem>
-                    <div className="w-1 h-12 bg-gray-300"></div>
-                    <TimelineHeader className="h-3">
-                        <TimelineIcon className="bg-blue-500 text-white">
-                            <ClockIcon className="h-5 w-5" />
-                        </TimelineIcon>
-                        <Typography variant="h6" color="blue-gray" className="leading-none">
-                            Pre-Taiwan
-                        </Typography>
-                    </TimelineHeader>
-                    <TimelineBody className="pb-8">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/30DBwAsJgM8?si=qRl_HZiOvWdgMcX_"
+
+        <div className="overflow-auto m-10 flex flex-col gap-y-10 h-screen">
+            <h1 className="flex text-6xl font-bold">Timeline</h1>
+            <ol className="items-center sm:flex">
+                <li className="relative mb-6 sm:mb-0">
+                    <div className="flex items-center">
+                        <div
+                            className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white sm:ring-8 shrink-0">
+                            <svg className="w-2.5 h-2.5 text-blue-800" aria-hidden="true"
+                                 xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path
+                                    d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                            </svg>
+                        </div>
+                        <div className="hidden sm:flex w-full bg-gray-200 h-0.5"></div>
+                    </div>
+                    <div className="mt-3 sm:pe-8">
+                        <h3 className="text-lg font-semibold text-gray-900">Equipment Haul</h3>
+                        <time
+                            className="block mb-5 text-sm font-normal leading-none text-gray-400">Pre-Taiwan
+                        </time>
+                        <iframe width="425" height="275"
+                                        src="https://www.youtube.com/embed/30DBwAsJgM8?si=qRl_HZiOvWdgMcX_"
+                                        title="YouTube video player" frameBorder="0"
+                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                        referrerPolicy="strict-origin-when-cross-origin"
+                                        allowFullScreen loading="lazy"></iframe>
+                    </div>
+                </li>
+                <li className="relative mb-6 sm:mb-0">
+                    <div className="flex items-center">
+                        <div
+                            className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white sm:ring-8 shrink-0">
+                            <svg className="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true"
+                                 xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path
+                                    d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                            </svg>
+                        </div>
+                        <div className="hidden sm:flex w-full bg-gray-200 h-0.5"></div>
+                    </div>
+                    <div className="mt-3 sm:pe-8">
+                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Getting Settled</h3>
+                        <time
+                            className="block mb-5 text-sm font-normal leading-none text-gray-400">1st Week
+                        </time>
+                        <iframe width="425" height="275"
+                                src="https://www.youtube.com/embed/30DBwAsJgM8?si=qRl_HZiOvWdgMcX_"
                                 title="YouTube video player" frameBorder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                 referrerPolicy="strict-origin-when-cross-origin"
                                 allowFullScreen loading="lazy"></iframe>
-                    </TimelineBody>
-                </TimelineItem>
-
-                <TimelineItem>
-                    <TimelineConnector className="h-12 w-0.5 bg-gray-300" />
-                    <TimelineHeader className="h-3">
-                        <TimelineIcon className="bg-blue-500 text-white">
-                            <ClockIcon className="h-5 w-5" />
-                        </TimelineIcon>
-                        <Typography variant="h6" color="blue-gray" className="leading-none">
-                            Landing
-                        </Typography>
-                    </TimelineHeader>
-                    <TimelineBody className="pb-8">
-                            <iframe width="560" height="315" src="https://www.youtube.com/embed/30DBwAsJgM8?si=qRl_HZiOvWdgMcX_"
-                                    title="YouTube video player" frameBorder="0"
-                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                    referrerPolicy="strict-origin-when-cross-origin"
-                                    allowFullScreen loading="lazy"></iframe>
-                        </TimelineBody>
-                </TimelineItem>
-
-                <TimelineItem>
-                    <TimelineConnector className="h-12 w-0.5 bg-gray-300" />
-                    <TimelineHeader className="h-3">
-                        <TimelineIcon className="bg-blue-500 text-white">
-                            <ClockIcon className="h-5 w-5" />
-                        </TimelineIcon>
-                        <Typography variant="h6" color="blue-gray" className="leading-none">
-                            Timeline Title Here.
-                        </Typography>
-                    </TimelineHeader>
-                    <TimelineBody className="pb-8">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/30DBwAsJgM8?si=qRl_HZiOvWdgMcX_"
+                    </div>
+                </li>
+                <li className="relative mb-6 sm:mb-0">
+                    <div className="flex items-center">
+                        <div
+                            className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white sm:ring-8 shrink-0">
+                            <svg className="w-2.5 h-2.5 text-blue-800" aria-hidden="true"
+                                 xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path
+                                    d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                            </svg>
+                        </div>
+                        <div className="hidden sm:flex w-full bg-gray-200 h-0.5"></div>
+                    </div>
+                    <div className="mt-3 sm:pe-8">
+                        <h3 className="text-lg font-semibold text-gray-900">Interviews & Surveys</h3>
+                        <time
+                            className="block mb-5 text-sm font-normal leading-none text-gray-400">2nd Week
+                        </time>
+                        <iframe width="425" height="275"
+                                src="https://www.youtube.com/embed/30DBwAsJgM8?si=qRl_HZiOvWdgMcX_"
                                 title="YouTube video player" frameBorder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                 referrerPolicy="strict-origin-when-cross-origin"
                                 allowFullScreen loading="lazy"></iframe>
-                    </TimelineBody>
-                </TimelineItem>
-
-                <TimelineItem>
-                    <TimelineConnector className="h-12 bg-gray-300" />
-                    <TimelineHeader className="h-3">
-                        <TimelineIcon className="bg-blue-500 text-white">
-                            <ClockIcon className="h-5 w-5" />
-                        </TimelineIcon>                        <Typography variant="h6" color="blue-gray" className="leading-none">
-                            Waishuangxi
-                        </Typography>
-                    </TimelineHeader>
-                    <TimelineBody className="pb-8">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/30DBwAsJgM8?si=qRl_HZiOvWdgMcX_"
+                    </div>
+                </li>
+                <li className="relative mb-6 sm:mb-0">
+                    <div className="flex items-center">
+                        <div
+                            className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white sm:ring-8 shrink-0">
+                            <svg className="w-2.5 h-2.5 text-blue-800" aria-hidden="true"
+                                 xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path
+                                    d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                            </svg>
+                        </div>
+                        <div className="hidden sm:flex w-full bg-gray-200 h-0.5"></div>
+                    </div>
+                    <div className="mt-3 sm:pe-8">
+                        <h3 className="text-lg font-semibold text-gray-900">Cultural Sites</h3>
+                        <time
+                            className="block mb-5 text-sm font-normal leading-none text-gray-400">3rd Week
+                        </time>
+                        <iframe width="425" height="275"
+                                src="https://www.youtube.com/embed/30DBwAsJgM8?si=qRl_HZiOvWdgMcX_"
                                 title="YouTube video player" frameBorder="0"
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                 referrerPolicy="strict-origin-when-cross-origin"
                                 allowFullScreen loading="lazy"></iframe>
-                    </TimelineBody>
-                </TimelineItem>
+                    </div>
+                </li>
+            </ol>
 
-                <TimelineItem>
-                    <TimelineHeader className="h-3">
-                        <TimelineIcon className="bg-blue-500 text-white">
-                            <ClockIcon className="h-5 w-5" />
-                        </TimelineIcon>                        <Typography variant="h6" color="blue-gray" className="leading-none">
-                            National Palace Museum
-                        </Typography>
-                    </TimelineHeader>
-                    <TimelineBody className="pb-8">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/30DBwAsJgM8?si=qRl_HZiOvWdgMcX_"
-                                title="YouTube video player" frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                referrerPolicy="strict-origin-when-cross-origin"
-                                allowFullScreen loading="lazy"></iframe>
-                    </TimelineBody>
-                </TimelineItem>
 
-            </Timeline>
         </div>
     );
 }
