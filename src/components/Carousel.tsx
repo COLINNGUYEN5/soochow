3e4a017ea513677c668a43117b0c43515a3507ca
cnguyen111 <cnguyen1@wpi.edu>
2025-03-18 23:36:28 -0400

    Fixing css

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,7 +1,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css"; // Import Swiper styles
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+// import "swiper/css"; // Import Swiper styles
+// import "swiper/css/navigation";
+// import "swiper/css/pagination";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import {Link} from "react-router-dom";
 
