4adf5679162fd4b111e2946d8237ecdccf78f662
cnguyen111 <cnguyen1@wpi.edu>
2025-04-18 21:30:45 -0400

    Trying to fix my pngs part 5

--- src/components/AdvisorCard.tsx
+++ src/components/AdvisorCard.tsx
@@ -1,11 +1,9 @@
-import grant from "../assets/drgrant.png"
-
 export function AdvisorCard() {
     return (
         <div className="flex flex-col md:flex-row justify-center gap-12">
 
             <div className="w-72 h-fit place-self-center">
-                <img src={grant} alt="profile-picture" className="h-70 w-full object-cover"/>
+                <img src="/people/drgrant.jpg" alt="profile-picture" className="h-70 w-full object-cover"/>
                 <div className="bg-white text-center py-2 shadow-md">
                     <p className="mb-2">
                         Dr. Grant Burrier
@@ -17,7 +15,7 @@ export function AdvisorCard() {
             </div>
 
             <div className="w-72 h-fit place-self-center">
-                <img src="/people/drphil.png" alt="profile-picture" className="h-70 w-full object-cover"/>
+                <img src="/people/drphil.jpg" alt="profile-picture" className="h-70 w-full object-cover"/>
                 <div className="bg-white text-center py-2 shadow-md">
                     <p className="mb-2">
                         Dr. Philip Hultquist
@@ -27,19 +25,6 @@ export function AdvisorCard() {
                     </p>
                 </div>
             </div>
-
-            <div className="w-72 h-fit place-self-center">
-                <img src="/people/drdu.jpg" alt="profile-picture" className="h-70 w-full object-cover"/>
-                <div className="bg-white text-center py-2 shadow-md">
-                    <p className="mb-2">
-                        Dr. Wen-Hua Du
-                    </p>
-                    <p className="font-medium blue-gray">
-                        Associate Professor of Teaching • Humanities
-                    </p>
-                </div>
-            </div>
-
         </div>
     );
 }
